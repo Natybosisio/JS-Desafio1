91bc670898776a63fd63e856b6028261112a419e
NataliaBosisio <nbosisio@hotmail.com>
2022-02-19 15:32:50 -0300

    Nuevos Cambios de identación

--- archivo.js
+++ archivo.js
@@ -14,12 +14,12 @@ cantidadReservas = parseInt(prompt("Cuantas reservas desea realizar?"));
 for(i = 0 ; i < cantidadReservas ; i++){
       
     function listado() {
-        dia = prompt("Elija el dia que desea alquilar:  \n - Lunes\n -Martes\n -Miercoles\n -Jueves\n-Viernes\n -Sabado");
+        dia =prompt("Elija el dia que desea alquilar:  \n - Lunes\n -Martes\n -Miercoles\n -Jueves\n-Viernes\n -Sabado");
 
         if(dia == "Lunes" || dia == "Martes" || dia == "Miercoles" || dia == "Jueves" || dia == "Viernes" || dia == "Sabado"){
-                horarios();
-                mensaje();
-                caja();
+             horarios();
+             mensaje();
+             caja();
         }
         else{
              alert("Ingrese una opcion correcta");
@@ -30,47 +30,47 @@ for(i = 0 ; i < cantidadReservas ; i++){
 
     function horarios() {
   
-            hora = prompt("Ingrese el horario que desea, entre las 8Am y las 22hs");
+        hora = prompt("Ingrese el horario que desea, entre las 8Am y las 22hs");
       
-            if (hora >= 8 && hora < 22) {
-                cantidadDeHoras = prompt("Cuantas horas desea alquilar?");
-                alert("El precio a abonar es de $" + precio * cantidadDeHoras);
-            }
-            else if (hora < 8 || hora >22) {
-                alert("Nos encontramos cerrados");
-                horarios();
-            }
-            else{
-                alert("Ingrese un horario valido");
-                horarios();
-            }
+        if (hora >= 8 && hora < 22) {
+            cantidadDeHoras = prompt("Cuantas horas desea alquilar?");
+            alert("El precio a abonar es de $" + precio * cantidadDeHoras);
+        }
+        else if (hora < 8 || hora >22) {
+            alert("Nos encontramos cerrados");
+            horarios();
+        }
+        else{
+            alert("Ingrese un horario valido");
+            horarios();
+        }
     }
 
     function mensaje(){
 
-       let confirmacion = prompt("desea confirmar? \n - Si\n -No");
+        let confirmacion = prompt("desea confirmar? \n - Si\n -No");
 
         switch(confirmacion){
        
             case "Si":
                alert("Usted reservo" + " " + dia + " " + hora + "hs, Gracias por elegirnos");
-            break;
+               break;
   
             case "No": 
                alert("Su reserva no fue registrada");
-            break;
+               break;
 
             default:
-             alert("La opcion no es correcta");
-              mensaje();
-            break;
+               alert("La opcion no es correcta");
+               mensaje();
+               break;
         }
 
     }
 
     function caja(){
-          let totalCaja = parseInt(precio * cantidadDeHoras);
-          console.log (" Ingreso a la caja" + " " + totalCaja);
+        let totalCaja = parseInt(precio * cantidadDeHoras);
+        console.log (" Ingreso a la caja" + " " + totalCaja);
     }
 
     listado();
